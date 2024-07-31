a5812dd118568faf120966f4d6921e8333cc03c0
testob02 <testob4real@gmail.com>
2024-07-31 08:38:18 +0100

    prac

--- Github/test.py
+++ Github/test.py
@@ -1,7 +1,7 @@
 def sum(x,y):
     return x + y
 
-inp1 = input('Enter first number:\t')
-inp2 = input('Enter second number:\t')
+inp1 = int(input('Enter first number:\t'))
+inp2 = int(input('Enter second number:\t'))
 
 print(inp1 + inp2)
