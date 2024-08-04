f78335de2654f3c20ffc7c14a0aaf6e4fe35ac57
testob02 <testob4real@gmail.com>
2024-08-04 21:30:13 +0100

    adding multiplayer game mode to tictactoe

--- Python/TicTacToe.py
+++ Python/TicTacToe.py
@@ -2,12 +2,32 @@ import random
 
 box_states = [i+1 for i in range(9)]
 
+
+def ask_game_mode():
+    game_mode = None
+    while game_mode != 1 or game_mode != 2:
+        game_mode = int(input("Which game mode:\nEnter 1 for vs CPU\nEnter 2 for vs player\n"))
+        if game_mode == 1 or game_mode == 2:
+            break
+    return game_mode
+
 def user_input():
     print('\n\n')
     user_inp = int(input("Enter your move:\t"))
     while check_if_box_filled(user_inp-1,box_states):
         user_inp = int(input("Enter your move:\t"))
     box_states[user_inp-1] = 'O'
+    print('\n\n')
+
+def multi_input(player_num):
+    print('\n\n')
+    user_inp = int(input(f"Player {player_num} Enter your move:\t"))
+    while check_if_box_filled(user_inp-1,box_states):
+        user_inp = int(input(f"Player {player_num} Enter your move:\t"))
+    if player_num == 1:
+        box_states[user_inp-1] = 'X'
+    elif player_num == 2:
+        box_states[user_inp-1] = 'O'
     print('\n\n')
 
 def get_computer_input():
@@ -72,6 +92,17 @@ def get_winner(box_states):
         return "User wins"
     elif state == 'Tie':
         return "Tie"
+    
+def get_multi_winner(box_states):
+    state = check_game_state(box_states)
+    if state == "game":
+        return
+    elif state == 'X':
+        return "Player 1 wins"
+    elif state == 'O':
+        return "Player 2 wins"
+    elif state == 'Tie':
+        return "Tie"
 
 def print_gui(box_states):
     print('*************************')
@@ -88,7 +119,7 @@ def print_gui(box_states):
     print('*\t*\t*\t*')
     print('*************************')
 
-def game_loop():
+def single_game_loop():
     while check_game_state(box_states) == 'game':
         get_computer_input()
         print_gui(box_states)
@@ -105,7 +136,29 @@ def game_loop():
             print(winner.upper())
             break
 
-game_loop()
+def multi_game_loop():
+    print_gui(box_states)
+    while check_game_state(box_states) == 'game':
+        multi_input(1)
+        print_gui(box_states)
+        winner = get_multi_winner(box_states)
+        if winner:
+            print('\n')
+            print(winner.upper())
+            break
+        multi_input(2)
+        print_gui(box_states)
+        winner = get_multi_winner(box_states)
+        if winner:
+            print('\n')
+            print(winner.upper())
+            break
+
+mode = ask_game_mode()
+if mode == 1:
+    single_game_loop()
+elif mode == 2:
+    multi_game_loop()
 
     
 
